412ccf93558b81b8f7ff3b2505c7ee71be309466
Galaletsang101 <morokonyana101@gmail.com>
2026-02-11 10:04:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function addInput(value) {
+document.getElementById("display").value += value;
+}
+function calculateResult (){
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+}
+function cleardisplay(){
+    document.getElementById('display').value='';
+}
+
+
+
+
